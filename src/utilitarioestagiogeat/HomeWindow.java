69c620a0a76bf7dc71ab00deb1b1d09801c4f438
Mathunes <matheusantunes720@gmail.com>
2020-12-10 18:37:25 -0300

    Add home window page

---
+++ src/utilitarioestagiogeat/HomeWindow.java
@@ -0,0 +1,34 @@
+package utilitarioestagiogeat;
+
+public class HomeWindow extends javax.swing.JFrame {
+
+    public HomeWindow() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(776, 468));
+        setPreferredSize(new java.awt.Dimension(776, 468));
+        setResizable(false);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
--- src/utilitarioestagiogeat/UtilitarioEstagioGeat.java
+++ src/utilitarioestagiogeat/UtilitarioEstagioGeat.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utilitarioestagiogeat;
 
-/**
- *
- * @author Felip
- */
 public class UtilitarioEstagioGeat {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        HomeWindow frame = new HomeWindow();
+        
+        frame.setVisible(true);
     }
     
 }
