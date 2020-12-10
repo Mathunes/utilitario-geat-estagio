f5b77c6d631aaf00fa5227f5ce6acb113ab0a13e
Mathunes <matheusantunes720@gmail.com>
2020-12-10 19:05:05 -0300

    Add title window and change panel background color

--- src/utilitarioestagiogeat/HomeWindow.java
+++ src/utilitarioestagiogeat/HomeWindow.java
@@ -1,6 +1,7 @@
 package utilitarioestagiogeat;
 
 import java.awt.Color;
+import java.awt.Font;
 
 public class HomeWindow extends javax.swing.JFrame {
 
@@ -14,21 +15,30 @@ public class HomeWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         homeWindowPanel = new javax.swing.JPanel();
+        titleWindow = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(776, 468));
         setPreferredSize(new java.awt.Dimension(776, 468));
         setResizable(false);
 
+        titleWindow.setText("Utilitário GEAT");
+
         javax.swing.GroupLayout homeWindowPanelLayout = new javax.swing.GroupLayout(homeWindowPanel);
         homeWindowPanel.setLayout(homeWindowPanelLayout);
         homeWindowPanelLayout.setHorizontalGroup(
             homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 776, Short.MAX_VALUE)
+            .addGroup(homeWindowPanelLayout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(titleWindow)
+                .addContainerGap(680, Short.MAX_VALUE))
         );
         homeWindowPanelLayout.setVerticalGroup(
             homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 468, Short.MAX_VALUE)
+            .addGroup(homeWindowPanelLayout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(titleWindow)
+                .addContainerGap(432, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -47,9 +57,14 @@ public class HomeWindow extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel homeWindowPanel;
+    private javax.swing.JLabel titleWindow;
     // End of variables declaration//GEN-END:variables
 
     private void stylizeComponentes() {
-        homeWindowPanel.setBackground(new Color(255, 187, 192));
+        homeWindowPanel.setBackground(new Color(232, 243, 240));
+        
+        titleWindow.setForeground(new Color(51, 51, 51));
+        titleWindow.setFont(new Font("Roboto", Font.BOLD, 28));
+        titleWindow.setLocation(185, 33);
     }
 }
