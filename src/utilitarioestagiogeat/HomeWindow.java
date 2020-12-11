07d4542e57a3ed5fbb3e3227a7807f6774c6d7bd
Mathunes <matheusantunes720@gmail.com>
2020-12-11 12:49:01 -0300

    Correct layout - add icons to buttons

--- src/utilitarioestagiogeat/HomeWindow.java
+++ src/utilitarioestagiogeat/HomeWindow.java
@@ -16,29 +16,83 @@ public class HomeWindow extends javax.swing.JFrame {
 
         homeWindowPanel = new javax.swing.JPanel();
         titleWindow = new javax.swing.JLabel();
+        btnDeleteUser = new javax.swing.JButton();
+        btnAlterUser = new javax.swing.JButton();
+        btnAddUserIBM = new javax.swing.JButton();
+        btnAddCC = new javax.swing.JButton();
+        btnAddUserOut = new javax.swing.JButton();
+        btnAddUser = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(776, 468));
-        setPreferredSize(new java.awt.Dimension(776, 468));
         setResizable(false);
 
+        homeWindowPanel.setMaximumSize(new java.awt.Dimension(776, 468));
+        homeWindowPanel.setMinimumSize(new java.awt.Dimension(776, 468));
+
         titleWindow.setText("Utilitário GEAT");
+
+        btnDeleteUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/utilitarioestagiogeat/btnDeleteUserIcon.png"))); // NOI18N
+
+        btnAlterUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/utilitarioestagiogeat/btnAlterUserIcon.png"))); // NOI18N
+        btnAlterUser.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAlterUserActionPerformed(evt);
+            }
+        });
+
+        btnAddUserIBM.setIcon(new javax.swing.ImageIcon(getClass().getResource("/utilitarioestagiogeat/btnAddUserIBMIcon.png"))); // NOI18N
+
+        btnAddCC.setIcon(new javax.swing.ImageIcon(getClass().getResource("/utilitarioestagiogeat/btnAddCCIcon.png"))); // NOI18N
+
+        btnAddUserOut.setIcon(new javax.swing.ImageIcon(getClass().getResource("/utilitarioestagiogeat/btnAddUserOutIcon.png"))); // NOI18N
+
+        btnAddUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/utilitarioestagiogeat/btnAddUserIcon.png"))); // NOI18N
+        btnAddUser.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddUserActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout homeWindowPanelLayout = new javax.swing.GroupLayout(homeWindowPanel);
         homeWindowPanel.setLayout(homeWindowPanelLayout);
         homeWindowPanelLayout.setHorizontalGroup(
             homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(homeWindowPanelLayout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(titleWindow)
-                .addContainerGap(680, Short.MAX_VALUE))
+                .addGap(34, 34, 34)
+                .addGroup(homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(titleWindow)
+                    .addGroup(homeWindowPanelLayout.createSequentialGroup()
+                        .addGroup(homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnAddUser, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnAddUserIBM, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(33, 33, 33)
+                        .addGroup(homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(homeWindowPanelLayout.createSequentialGroup()
+                                .addComponent(btnAddCC, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(33, 33, 33)
+                                .addComponent(btnAddUserOut, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(homeWindowPanelLayout.createSequentialGroup()
+                                .addComponent(btnDeleteUser, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnAlterUser, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGap(40, 40, 40))
         );
         homeWindowPanelLayout.setVerticalGroup(
             homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(homeWindowPanelLayout.createSequentialGroup()
                 .addGap(22, 22, 22)
                 .addComponent(titleWindow)
-                .addContainerGap(432, Short.MAX_VALUE))
+                .addGap(29, 29, 29)
+                .addGroup(homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAddUser, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnDeleteUser, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAlterUser, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(33, 33, 33)
+                .addGroup(homeWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAddUserIBM, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAddCC, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAddUserOut, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(64, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -55,7 +109,22 @@ public class HomeWindow extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAddUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddUserActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAddUserActionPerformed
+
+    private void btnAlterUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAlterUserActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAlterUserActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddCC;
+    private javax.swing.JButton btnAddUser;
+    private javax.swing.JButton btnAddUserIBM;
+    private javax.swing.JButton btnAddUserOut;
+    private javax.swing.JButton btnAlterUser;
+    private javax.swing.JButton btnDeleteUser;
     private javax.swing.JPanel homeWindowPanel;
     private javax.swing.JLabel titleWindow;
     // End of variables declaration//GEN-END:variables
@@ -63,8 +132,32 @@ public class HomeWindow extends javax.swing.JFrame {
     private void stylizeComponentes() {
         homeWindowPanel.setBackground(new Color(232, 243, 240));
         
-        titleWindow.setForeground(new Color(51, 51, 51));
+        titleWindow.setForeground(new Color(5, 51, 51));
         titleWindow.setFont(new Font("Roboto", Font.BOLD, 28));
-        titleWindow.setLocation(185, 33);
+        titleWindow.setBounds(35, 31, titleWindow.getPreferredSize().width, titleWindow.getPreferredSize().height);
+        
+        btnAddUser.setLocation(35, 92);
+        btnAddUser.setBackground(new Color(255, 255, 255));
+        btnAddUser.setFocusable(false);
+        
+        btnDeleteUser.setLocation(282, 92);
+        btnDeleteUser.setBackground(new Color(255, 255, 255));
+        btnDeleteUser.setFocusable(false);
+        
+        btnAlterUser.setLocation(529, 92);
+        btnAlterUser.setBackground(new Color(255, 255, 255));
+        btnAlterUser.setFocusable(false);
+        
+        btnAddUserIBM.setLocation(35, 280);
+        btnAddUserIBM.setBackground(new Color(255, 255, 255));
+        btnAddUserIBM.setFocusable(false);
+        
+        btnAddCC.setLocation(282, 280);
+        btnAddCC.setBackground(new Color(255, 255, 255));
+        btnAddCC.setFocusable(false);
+        
+        btnAddUserOut.setLocation(529, 280);
+        btnAddUserOut.setBackground(new Color(255, 255, 255));
+        btnAddUserOut.setFocusable(false);
     }
 }
