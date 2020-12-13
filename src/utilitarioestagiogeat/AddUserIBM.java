05b3812ea2af92ffb811672f2702afa6e25cae32
Mathunes <matheusantunes720@gmail.com>
2020-12-13 13:11:19 -0300

    Add add user ibm window

---
+++ src/utilitarioestagiogeat/AddUserIBM.java
@@ -0,0 +1,184 @@
+package utilitarioestagiogeat;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class AddUserIBM extends javax.swing.JFrame {
+
+
+    public AddUserIBM() {
+        initComponents();
+        stylizeComponentes();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        addUserIBMWindowPanel = new javax.swing.JPanel();
+        titleWindow = new javax.swing.JLabel();
+        btnClean = new javax.swing.JButton();
+        btnConvert = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField7 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField9 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        addUserIBMWindowPanel.setMaximumSize(new java.awt.Dimension(776, 468));
+        addUserIBMWindowPanel.setMinimumSize(new java.awt.Dimension(776, 468));
+
+        titleWindow.setText("Criação de usuário IBM");
+
+        btnClean.setText("Limpar");
+
+        btnConvert.setText("Gerar");
+
+        jLabel1.setText("ID do usuário");
+
+        jLabel2.setText("name_tso");
+
+        jLabel4.setText("name_complete");
+
+        jLabel5.setText("Lotação");
+
+        jLabel6.setText("Senha");
+
+        jLabel3.setText("Grupo");
+
+        javax.swing.GroupLayout addUserIBMWindowPanelLayout = new javax.swing.GroupLayout(addUserIBMWindowPanel);
+        addUserIBMWindowPanel.setLayout(addUserIBMWindowPanelLayout);
+        addUserIBMWindowPanelLayout.setHorizontalGroup(
+            addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(addUserIBMWindowPanelLayout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnConvert, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, addUserIBMWindowPanelLayout.createSequentialGroup()
+                        .addComponent(titleWindow)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 529, Short.MAX_VALUE)
+                        .addComponent(btnClean))
+                    .addGroup(addUserIBMWindowPanelLayout.createSequentialGroup()
+                        .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel5))
+                        .addGap(29, 29, 29)
+                        .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jTextField3)
+                            .addComponent(jTextField7)
+                            .addGroup(addUserIBMWindowPanelLayout.createSequentialGroup()
+                                .addComponent(jTextField9, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel3)
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addGap(37, 37, 37))
+        );
+        addUserIBMWindowPanelLayout.setVerticalGroup(
+            addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(addUserIBMWindowPanelLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(titleWindow)
+                    .addComponent(btnClean))
+                .addGap(18, 18, 18)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField7, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(17, 17, 17)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(addUserIBMWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jTextField9, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addComponent(btnConvert, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(addUserIBMWindowPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(addUserIBMWindowPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 366, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AddUserIBM().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel addUserIBMWindowPanel;
+    private javax.swing.JButton btnClean;
+    private javax.swing.JButton btnConvert;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField7;
+    private javax.swing.JTextField jTextField9;
+    private javax.swing.JLabel titleWindow;
+    // End of variables declaration//GEN-END:variables
+
+        private void stylizeComponentes() {
+        addUserIBMWindowPanel.setBackground(new Color(232, 243, 240));
+        
+        titleWindow.setForeground(new Color(51, 51, 51));
+        titleWindow.setFont(new Font("Roboto", Font.BOLD, 20));
+        titleWindow.setBounds(35, 31, titleWindow.getPreferredSize().width, titleWindow.getPreferredSize().height);
+        
+        btnConvert.setBackground(new Color(17, 125, 125));
+        btnConvert.setFocusable(false);
+        btnConvert.setFont(new Font("Roboto", Font.BOLD, 18));
+        btnConvert.setForeground(new Color(255, 255, 255));
+        
+        btnClean.setBackground(new Color(255, 72, 90));
+        btnClean.setFocusable(false);
+        btnClean.setFont(new Font("Roboto", Font.BOLD, 14));
+        btnClean.setForeground(new Color(255, 255, 255));
+        
+    }
+}
