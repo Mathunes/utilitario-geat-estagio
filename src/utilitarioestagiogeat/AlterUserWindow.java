77683d8965182abfe57bd5ec1e0b53d0ebb41efd
Mathunes <matheusantunes720@gmail.com>
2020-12-14 22:39:33 -0300

    Add function to build result

--- src/utilitarioestagiogeat/AlterUserWindow.java
+++ src/utilitarioestagiogeat/AlterUserWindow.java
@@ -3,6 +3,7 @@ package utilitarioestagiogeat;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Toolkit;
+import javax.swing.JOptionPane;
 
 public class AlterUserWindow extends javax.swing.JFrame {
 
@@ -20,13 +21,13 @@ public class AlterUserWindow extends javax.swing.JFrame {
         alterUserWindowPanel = new javax.swing.JPanel();
         titleWindow = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        inputTextPane = new javax.swing.JTextPane();
+        textArea = new javax.swing.JTextPane();
         btnClean = new javax.swing.JButton();
         btnConvert = new javax.swing.JButton();
         btnHomeWindow = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        radioSamu = new javax.swing.JRadioButton();
+        radioMainframe = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(776, 468));
@@ -37,11 +38,21 @@ public class AlterUserWindow extends javax.swing.JFrame {
 
         titleWindow.setText("Alteração de pessoal");
 
-        jScrollPane1.setViewportView(inputTextPane);
+        jScrollPane1.setViewportView(textArea);
 
         btnClean.setText("Limpar");
+        btnClean.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCleanActionPerformed(evt);
+            }
+        });
 
         btnConvert.setText("Converter");
+        btnConvert.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConvertActionPerformed(evt);
+            }
+        });
 
         btnHomeWindow.setText("Página Inicial");
         btnHomeWindow.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -52,13 +63,13 @@ public class AlterUserWindow extends javax.swing.JFrame {
 
         jLabel1.setText("Fonte de dados:");
 
-        jRadioButton1.setBackground(new java.awt.Color(232, 243, 240));
-        dataFontGroup.add(jRadioButton1);
-        jRadioButton1.setText("Relatório SAMU");
+        radioSamu.setBackground(new java.awt.Color(232, 243, 240));
+        dataFontGroup.add(radioSamu);
+        radioSamu.setText("Relatório SAMU");
 
-        jRadioButton2.setBackground(new java.awt.Color(232, 243, 240));
-        dataFontGroup.add(jRadioButton2);
-        jRadioButton2.setText("Relatório Mainframe");
+        radioMainframe.setBackground(new java.awt.Color(232, 243, 240));
+        dataFontGroup.add(radioMainframe);
+        radioMainframe.setText("Relatório Mainframe");
 
         javax.swing.GroupLayout alterUserWindowPanelLayout = new javax.swing.GroupLayout(alterUserWindowPanel);
         alterUserWindowPanel.setLayout(alterUserWindowPanelLayout);
@@ -70,9 +81,9 @@ public class AlterUserWindow extends javax.swing.JFrame {
                     .addGroup(alterUserWindowPanelLayout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(18, 18, 18)
-                        .addComponent(jRadioButton1)
+                        .addComponent(radioSamu)
                         .addGap(18, 18, 18)
-                        .addComponent(jRadioButton2)
+                        .addComponent(radioMainframe)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(alterUserWindowPanelLayout.createSequentialGroup()
                         .addGroup(alterUserWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -99,8 +110,8 @@ public class AlterUserWindow extends javax.swing.JFrame {
                 .addGap(14, 14, 14)
                 .addGroup(alterUserWindowPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2))
+                    .addComponent(radioSamu)
+                    .addComponent(radioMainframe))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnConvert, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(38, Short.MAX_VALUE))
@@ -129,13 +140,123 @@ public class AlterUserWindow extends javax.swing.JFrame {
         frame.setVisible(true);
     }//GEN-LAST:event_btnHomeWindowMouseClicked
 
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AlterUserWindow().setVisible(true);
+    private void btnConvertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConvertActionPerformed
+        if (radioMainframe.isSelected()) {
+        
+            String textInput = textArea.getText().trim();
+            textArea.setText("");
+
+            for (int i = 0; i < textInput.length(); i++) {
+
+                String userId = "";
+                String userInfo = "";
+                String lotacao = "";
+
+                while ((textInput.charAt(i) != '\n') && (i < textInput.length()-1)) {
+                    userInfo += textInput.charAt(i);
+                    i++;
+                }
+
+                for (int j = 0; j < 11; j++) {
+                    if (userInfo.charAt(j) != ' ') {
+                        userId += userInfo.charAt(j);
+                    }
+                }
+
+                for (int j = 61; j < userInfo.length(); j++) {
+                    lotacao += userInfo.charAt(j);
+                }
+
+                textArea.setText(textArea.getText() + "- username: " + userId + "\n  org_unit: " + lotacao + "\n");
             }
-        });
-    }
+        } else if (radioSamu.isSelected()) {
+            
+            String textInput = textArea.getText().trim();
+            textArea.setText("");
+            String userId = "";
+            
+            if (textInput.contains("Usuários desligados:")) {
+                int indiceDesligado = textInput.indexOf("Usuários desligados:");
+                
+                String infoDesligado = "";
+                
+                for (int i = indiceDesligado; i < textInput.length(); i++) {
+                    infoDesligado += textInput.charAt(i);
+                }
+                
+                textInput = textInput.replace(infoDesligado, "");
+                
+            }
+            
+            
+            
+            
+            int i = textInput.indexOf("Usuários alterados:") + "Usuários alterados:\n".length();
+            
+            for (; i < textInput.length(); i++) {
+                
+                String userInfo = "";
+                String lotacao = "";
+                boolean erroLotacao = false;
+                
+                while ((textInput.charAt(i) != '\n') && (i < textInput.length()-1)) {
+                    userInfo += textInput.charAt(i);
+                    i++;
+                }
+                
+                if (userInfo.contains("User ID.............. ")) {
+                    userId = "";
+                    userId = userInfo.replace("User ID.............. ", "").toUpperCase();
+                }
+                
+                if (userInfo.contains("Lotação.............. ")) {
+                    erroLotacao = true;
+                    
+                    lotacao = userInfo.replace("Lotação.............. ", "");
+                    
+                    int j = lotacao.indexOf("->");
+                    
+                    String aux = "";
+                    
+                    for (int k = 0; k < j; k++) {
+                        aux += lotacao.charAt(k);
+                    }
+                    
+                    lotacao = lotacao.replace(aux + "-> ", "");
+                    
+                    if (lotacao.contains("/")) {
+
+                        int firstBar = lotacao.indexOf('/');
+                        int lastBar = lotacao.lastIndexOf('/');
+
+                        aux = "";
+
+                        for (int k = firstBar; k <= lastBar; k++) {
+                            aux += lotacao.charAt(k);
+                        }
+
+                        lotacao = lotacao.replace(aux, "-");
+
+                    }
+                    
+                }
+                
+                if (erroLotacao) {
+                    textArea.setText(textArea.getText() + "- username: " + userId + "\n  org_unit: " + lotacao + "\n");
+                    erroLotacao = false;
+                }
+                
+            }
+            
+            
+        } else {
+            JOptionPane.showMessageDialog(rootPane, "Selecione uma fonte de dados!", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_btnConvertActionPerformed
+
+    private void btnCleanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCleanActionPerformed
+        textArea.setText("");
+    }//GEN-LAST:event_btnCleanActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel alterUserWindowPanel;
@@ -143,11 +264,11 @@ public class AlterUserWindow extends javax.swing.JFrame {
     private javax.swing.JButton btnConvert;
     private javax.swing.JLabel btnHomeWindow;
     private javax.swing.ButtonGroup dataFontGroup;
-    private javax.swing.JTextPane inputTextPane;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JRadioButton radioMainframe;
+    private javax.swing.JRadioButton radioSamu;
+    private javax.swing.JTextPane textArea;
     private javax.swing.JLabel titleWindow;
     // End of variables declaration//GEN-END:variables
 
@@ -171,8 +292,8 @@ public class AlterUserWindow extends javax.swing.JFrame {
         btnHomeWindow.setFont(new Font("Roboto", Font.BOLD, 14));
         btnHomeWindow.setForeground(new Color(44, 94, 95));
         
-        jRadioButton1.setFocusable(false);
-        jRadioButton2.setFocusable(false);
+        radioSamu.setFocusable(false);
+        radioMainframe.setFocusable(false);
         
         setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("icon.png")));        
     }
