96ffd430e6c5641808bae5cc6d8d725c05c7d72e
Mathunes <matheusantunes720@gmail.com>
2020-12-25 15:21:27 -0300

    Change version

--- src/utilitarioestagiogeat/HomeWindow.java
+++ src/utilitarioestagiogeat/HomeWindow.java
@@ -86,7 +86,7 @@ public class HomeWindow extends javax.swing.JFrame {
             }
         });
 
-        version.setText("Versão 1.1");
+        version.setText("Versão 1.2");
 
         javax.swing.GroupLayout homeWindowPanelLayout = new javax.swing.GroupLayout(homeWindowPanel);
         homeWindowPanel.setLayout(homeWindowPanelLayout);
